1fdc3a3c1dd71bf7cf18122c9954ac8bd2fef751
Pranav006svg <mppranavkumar06@gmail.com>
2026-04-17 22:56:41 +0530

    UC6: Queue + Stack Based Palindrome Check

---
+++ PalindromeCheckerApp/src/UseCase6PalindromeCheckerApp.java
@@ -0,0 +1,59 @@
+/**
+ * ============================================================
+ * MAIN CLASS - UseCase6PalindromeCheckerApp
+ * ============================================================
+ *
+ * Use Case 6: Queue + Stack Fairness Check
+ *
+ * Description:
+ * This class demonstrates palindrome validation using
+ * two different data structures:
+ *
+ * - Queue (FIFO - First In First Out)
+ * - Stack (LIFO - Last In First Out)
+ *
+ * Characters are inserted into both structures and then
+ * compared by removing from the front of the queue and
+ * the top of the stack.
+ *
+ * If all characters match, the input string is confirmed
+ * as a palindrome.
+ *
+ * This use case helps understand how FIFO and LIFO
+ * behaviors can be combined for symmetric comparison.
+ *
+ * @author Developer
+ * @version 6.0
+ */
+
+import java.util.*;
+
+public class UseCase6PalindromeCheckerApp {
+
+
+    public static void main(String[] args) {
+
+        String input = "civic";
+
+        Queue<Character> queue = new LinkedList<>();
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : input.toCharArray()) {
+            queue.add(c);
+            stack.push(c);
+        }
+
+        boolean isPalindrome = true;
+
+        while (!queue.isEmpty()) {
+            if (!queue.poll().equals(stack.pop())) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+    }
+}
